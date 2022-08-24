11e1e96a342e15b74a818504979aa4fb147735a7
StirringWizard1 <100811927+StirringWizard1@users.noreply.github.com>
2022-08-24 23:38:49 +0930

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "EfficientTruckloads.h"
+//#include "Truckloads.h"
+
+int main()
+{
+	
+	//  std::string test = "hello";
+	//  int test2 = 112345;
+
+
+	// Reverse Test; 
+
+
+	// std::cout << Test.reverseString(test) << std::endl;
+	// std::cout << Test.reverseDigit(test2) <<std::endl;
+
+	int A = 24;
+	int B = 3;
+
+	EfficientTruckloads HI; 
+	std::cout << HI.numTrucks (A,B)<< std::endl;
+
+
+
+
+
+}
